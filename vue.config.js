bfa4e0e95d1842c7bf786edd09a53dcfd61ea563
Eduard.Zeleniuk <eduard.zeleniuk@gmail.com>
2019-02-11 19:25:49 +0200

    git init

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = {
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: [path.resolve(__dirname, "./src/styles/global.scss")]
+    }
+  },
+  chainWebpack: config => {
+    config.module
+      .rule("vue")
+      .use("vue-svg-inline-loader")
+      .loader("vue-svg-inline-loader")
+      .options({ /* ... */ });
+  }
+}
